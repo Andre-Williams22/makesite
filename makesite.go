8867978012ae8e1323bb81cbd7d35f1fc34961cb
Andre Williams <awjrs22@gmail.com>
2021-02-04 22:44:28 -0500

    run a directory

--- makesite.go
+++ makesite.go
@@ -9,8 +9,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"strings"
+	// "string"
 )
 
 // Storing Data
@@ -24,15 +28,50 @@ type ToDo struct {
 	List []entry
 }
 
-// func main() {
-// 	t := template.Must(template.New("template.tmpl").ParseFiles("new.html"))
-// 	err = t.Execute(os.Stdout, ToDo.User)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func main() {
+	// t := template.Must(template.New("template.tmpl").ParseFiles("new.html"))
+	// err = t.Execute(os.Stdout, ToDo.User)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-func readFile() {
+	fileFlag := flag.String("file", "first-post.txt", "define input text")
+	dirFlag := flag.String("directory", "none", "generates all .txt files in directory")
+	outputDirFlag := flag.String("output", "templates/", "Generator output directory")
+	flag.Parse()
+
+	if *dirFlag == "none" {
+		runFile(*fileFlag, "txt_dir/")
+	} else {
+		runDir(*dirFlag, *outputDirFlag)
+	}
+}
+
+func runFile(fileFlag, directory string) {
+
+	var fileName string = fileFlag
+
+	if fileName[strings.Index(fileFlag, "."):len(fileFlag)] != ".txt" {
+		return
+	}
+
+	if strings.Contains(strings.ToLower(fileFlag), ".md") {
+
+		var data string = readFile(directory + fileFlag)
+		tmpl := renderTemplate("template.tmpl", data, fileName)
+		output := blackfriday.Run(tmpl)
+		ioutil.WriteFile(output, fileFlag)
+
+		return
+	}
+
+	fileName = fileName[0:strings.Index(fileFlag, ".")] + ".html"
+
+	var data string = readFile(directory + fileFlag)
+	renderTemplate("template.tmpl", data, fileName)
+}
+
+func readFile(file string) string {
 	fileContents, err := ioutil.ReadFile("first-post.txt")
 	if err != nil {
 		// A common use of `panic` is to abort if a function returns an error
@@ -41,13 +80,34 @@ func readFile() {
 		panic(err)
 	}
 	fmt.Print(string(fileContents))
+	return string(fileContents)
 }
 
-func writeFile() {
+func writeFile(file string, content string) {
 	bytesToWrite := []byte("hello\ngo\n")
 	err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func saveFile() {
+	var filename string
+	var dir string
+
+	flag.StringVar(&filename, "file", "", "File name")
+	flag.StringVar(&dir, "dir", "", "Directory name")
+	flag.Parse()
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		if f.Name()[len(f.Name())-4:] == ".txt" {
+			content := readFile(dir + "/" + f.Name())
+			writeFile(f.Name(), content)
+		}
 	}
 }
 
@@ -65,3 +125,7 @@ func writeFile() {
 
 // Run latest Build
 // ./makesite or $ ./(name of go file)
+
+// go build -o main
+// ./main
+// go run main.go
